84c65ae3385b8f872665945eeabcc03197261b70
v-xchen-v <407081305@qq.com>
2023-11-06 16:50:33 +0800

    add test of k factor influences on ranking and same result with arena with K = 4

--- arena_data/__init__.py
+++ arena_data/__init__.py
@@ -1,1 +1,1 @@
-from arena_data.arena_helper import get_arena_battles_data, get_arena_battles_models
+from arena_data.arena_helper import get_arena_battles_data, get_arena_battles_models, get_arena_battle_res_dict
